51365b3cd91503fc10aa5e23349c8130ab90b577
Keerthivarman608 <keerthivarman608@gmail.com>
2026-03-19 16:20:48 +0530

    Fix PWABuilder 100/100 compliance

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+// Basic Service Worker for PWABuilder caching compliance
+const CACHE_NAME = 'dinedesk-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request);
+      })
+  );
+});
